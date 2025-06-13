eefe51b50bc5ff5187264b63ade9aba1c798a7ff
alexbaumgertner <alex.baumgertner@gmail.com>
2025-06-13 13:41:43 +0300

    Make the warmup task more complex and interesting

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tasks for FE/JS/TS full stack interview live coding
+
+Repo: [https://github.com/easy-deep-learning/interview-tasks](https://github.com/easy-deep-learning/interview-tasks)
---
+++ strings-arrays-objects-functions-promises.js
@@ -0,0 +1,83 @@
+// TASK
+// Job offers management system
+const jobOffers = [
+  {
+    id: 1,
+    title: "Frontend Developer",
+    location: "Texas, USA",
+    salary: "$100000",
+    skills: ["JavaScript", "React", "CSS"],
+  },
+  {
+    id: 2,
+    title: "Full Stack Engineer",
+    location: "London, UK",
+    salary: "£80000",
+    skills: ["JavaScript", "Node.js", "Express"],
+  },
+  {
+    id: 3,
+    title: "React Engineer",
+    location: "Sidney, Australia",
+    salary: "$90000",
+    skills: ["JavaScript", "React", "Redux"],
+  },
+  {
+    id: 4,
+    title: "Next.js Developer",
+    location: "Berlin, Germany",
+    salary: "€110000",
+    skills: ["JavaScript", "Next.js", "TypeScript"],
+  },
+  {
+    id: 5,
+    title: "Full Stack Engineer (TypeScript, AWS)",
+    location: "Paris, France",
+    salary: "$120000",
+    skills: ["TypeScript", "AWS", "Node.js"],
+  },
+];
+
+
+// Return an array of job titles that require React skills
+function findReactJobs(jobs) {
+  // Your code here
+  return 'TODO'
+}
+
+console.log(findReactJobs(jobOffers))
+// => ['Frontend Developer', 'React Engineer']
+
+
+
+// TASK
+// Get data for Job Offers list page: request ob Offers and User Profile
+
+const api = [
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(jobOffers);
+    }, 30);
+  }),
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        userId: 123,
+        name: "John Doe",
+        email: "example@example.com"
+      });
+    }, 50);
+  })
+];
+
+// Fetch job offers and user profile data
+async function fetchJobOffersAndUserProfile() {
+  // Q: How to fetch data from both promises in parallel?
+  // Q: What to do if one of the promises fails?
+  // Q: What if we want to return both success and fail results in a single object?
+  // Q: How to handle errors in async functions?
+  return 'TODO'
+}
+
+// Example usage
+// log the result of the function
